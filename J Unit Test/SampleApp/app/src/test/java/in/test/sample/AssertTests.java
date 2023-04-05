69574499381b561edfc9fbd5911674407cc8d7cb
MuammarYasir <72369133+MuammarYasir@users.noreply.github.com>
2023-04-05 14:27:08 +0700

    J Unit Test

--- J Unit Test/SampleApp/app/src/test/java/in/test/sample/AssertTests.java
+++ J Unit Test/SampleApp/app/src/test/java/in/test/sample/AssertTests.java
@@ -25,9 +25,6 @@ import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by Ravindra Kumar on 16/09/16.
- */
 
 public class AssertTests {
     @Test
